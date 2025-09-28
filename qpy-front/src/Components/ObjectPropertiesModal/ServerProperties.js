a9fbc4ccc050435f7fa190dba9a73ec4bc8941fe
PedroMion <pedromionbraga@gmail.com>
2025-09-28 19:15:01 -0300

    Adjusting modal style

--- qpy-front/src/Components/ObjectPropertiesModal/ServerProperties.js
+++ qpy-front/src/Components/ObjectPropertiesModal/ServerProperties.js
@@ -3,19 +3,36 @@ import './ServerProperties.css';
 function ServerProperties() {
   return (
     <div className="server-properties-container">
+      <div className='server-properties-header'>
+        <div className='server-properties-header-text'>Configure Server</div>
+        <div className='server-properties-header-button'>X</div>
+      </div>
+
       <div className='server-properties-element'>
         <span className='server-properties-element-text'>Service Rate (Jobs per second):</span>
-        <input></input>
+        <input type='number' className='server-properties-element-input'></input>
       </div>
 
       <div className='server-properties-element'>
         <span className='server-properties-element-text'>Service Distribution:</span>
-        <input></input>
+        <select className='server-properties-element-select'>
+          <option>Exponential</option>
+          <option>Constant</option>
+          <option>Uniform</option>
+          <option>Normal</option>
+        </select>
       </div>
 
       <div className='server-properties-element'>
         <span className='server-properties-element-text'>Queue Discipline:</span>
-        <input></input>
+        <select className='server-properties-element-select'>
+          <option>First Come First Served</option>
+          <option>Last Come First Served</option>
+          <option>Round Robin</option>
+          <option>Shortest Remaining Time</option>
+          <option>Normal</option>
+          <option>Priority</option>
+        </select>
       </div>
 
       <div className='server-properties-button-container'>
