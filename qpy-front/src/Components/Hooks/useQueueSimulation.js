b80b102dd1914737cd35e00d557a8717ce40229f
PedroMion <pedromionbraga@gmail.com>
2025-10-15 09:27:26 -0300

    Fix identation

--- qpy-front/src/Components/Hooks/useQueueSimulation.js
+++ qpy-front/src/Components/Hooks/useQueueSimulation.js
@@ -3,31 +3,31 @@ import { useObjectPropertiesModals } from './useObjectPropertiesModals';
 import { useNetworkConfiguration } from './useNetworkConfiguration';
 
 export const useQueueSimulation = (nodes, edges) => {
-    const {
-      getNetworkConfiguration,
-    } = useNetworkConfiguration();
+  const {
+    getNetworkConfiguration,
+  } = useNetworkConfiguration();
 
-    const [simulationResults, setSimulationResults] = useState({});
+  const [simulationResults, setSimulationResults] = useState({});
 
-    const {
-      showLoading,
-      finishLoading,
-      onShowResultsModal,
-    } = useObjectPropertiesModals();
-  
-    const simulate = () => {
+  const {
+    showLoading,
+    finishLoading,
+    onShowResultsModal,
+  } = useObjectPropertiesModals();
+
+  const simulate = () => {
     showLoading();
-    
+
     const requestJson = mapVariablesToJsonRequest(nodes, edges);
     const response = sendRequestToQpy(requestJson);
-    
+
     finishLoading();
 
     if(isResponseValid(response)) {
-        setSimulationResults(response.json)
-        onShowResultsModal();
+      setSimulationResults(response.json)
+      onShowResultsModal();
     } else {
-        showErrorToUser(response);
+      showErrorToUser(response);
     };
   };
 
@@ -99,9 +99,9 @@ export const useQueueSimulation = (nodes, edges) => {
     var connections = getConnectionsFromEdgesData(edges);
 
     return {
-        "networkConfiguration": getNetworkConfiguration(),
-        "devices": devices,
-        "connections": connections
+      "networkConfiguration": getNetworkConfiguration(),
+      "devices": devices,
+      "connections": connections
     }
   };
 
@@ -109,19 +109,19 @@ export const useQueueSimulation = (nodes, edges) => {
     var devices = {"servers": [], "arrivals": []};
 
     for(const node of nodes) {
-        if(node.type === "server") {
-            devices.servers.push({
-                "device_id": node.id,
-                "distribution": node.data.distributionProperties,
-                "queue": node.data.queueDiscipline,
-            })
-        } else {
-            devices.arrivals.push({
-                "device_id": node.id,
-                "distribution": node.data.distributionProperties,
-                "priorityDistribution": node.data.priorityDistribution,
-            })
-        }
+      if(node.type === "server") {
+        devices.servers.push({
+          "device_id": node.id,
+          "distribution": node.data.distributionProperties,
+          "queue": node.data.queueDiscipline,
+        })
+      } else {
+        devices.arrivals.push({
+          "device_id": node.id,
+          "distribution": node.data.distributionProperties,
+          "priorityDistribution": node.data.priorityDistribution,
+        })
+      }
     }
 
     return devices;
@@ -131,11 +131,11 @@ export const useQueueSimulation = (nodes, edges) => {
     var connections = [];
 
     for(const edge of edges) {
-        connections.push({
-            "source": edge.source,
-            "target": edge.target,
-            "routingProbability": edge.data.routingProbability,
-        });
+      connections.push({
+        "source": edge.source,
+        "target": edge.target,
+        "routingProbability": edge.data.routingProbability,
+      });
     }
 
     return connections;
